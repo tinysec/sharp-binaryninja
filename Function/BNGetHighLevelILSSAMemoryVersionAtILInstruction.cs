050d0e43d8cbc972ca28ebad11080c0bc4f7bcb4
tinysec <root@tinysec.net>
2026-07-22 21:22:48 +0800

    feat: complete HLIL function navigation

--- Function/BNGetHighLevelILSSAMemoryVersionAtILInstruction.cs
+++ Function/BNGetHighLevelILSSAMemoryVersionAtILInstruction.cs
@@ -1,27 +1,16 @@
 using System;
-using System.Collections.Generic;
 using System.Runtime.InteropServices;
-using Microsoft.Win32.SafeHandles;
 
 namespace BinaryNinja
 {
-    internal static partial class NativeMethods
-    {
-	    /// <summary>
-		/// uint64_t BNGetHighLevelILSSAMemoryVersionAtILInstruction(BNHighLevelILFunction* func, uint64_t instr)
-		/// </summary>
+	internal static partial class NativeMethods
+	{
 		[DllImport(
-            "binaryninjacore", 
-            CallingConvention = System.Runtime.InteropServices.CallingConvention.Cdecl,
-            EntryPoint = "BNGetHighLevelILSSAMemoryVersionAtILInstruction"
-        )]
+			"binaryninjacore",
+			CallingConvention = System.Runtime.InteropServices.CallingConvention.Cdecl,
+			EntryPoint = "BNGetHighLevelILSSAMemoryVersionAtILInstruction")]
 		internal static extern ulong BNGetHighLevelILSSAMemoryVersionAtILInstruction(
-			
-			// BNHighLevelILFunction* func
-		    IntPtr func  , 
-			
-			// uint64_t instr
-			HighLevelILInstructionIndex instr  
-		);
+			IntPtr function,
+			HighLevelILInstructionIndex instruction);
 	}
 }
--- Function/BNGetHighLevelILSSAVarVersionAtILInstruction.cs
+++ Function/BNGetHighLevelILSSAVarVersionAtILInstruction.cs
@@ -1,30 +1,17 @@
 using System;
-using System.Collections.Generic;
 using System.Runtime.InteropServices;
-using Microsoft.Win32.SafeHandles;
 
 namespace BinaryNinja
 {
-    internal static partial class NativeMethods
-    {
-	    /// <summary>
-		/// uint64_t BNGetHighLevelILSSAVarVersionAtILInstruction(BNHighLevelILFunction* func, BNVariable* var, uint64_t instr)
-		/// </summary>
+	internal static partial class NativeMethods
+	{
 		[DllImport(
-            "binaryninjacore", 
-            CallingConvention = System.Runtime.InteropServices.CallingConvention.Cdecl,
-            EntryPoint = "BNGetHighLevelILSSAVarVersionAtILInstruction"
-        )]
+			"binaryninjacore",
+			CallingConvention = System.Runtime.InteropServices.CallingConvention.Cdecl,
+			EntryPoint = "BNGetHighLevelILSSAVarVersionAtILInstruction")]
 		internal static extern ulong BNGetHighLevelILSSAVarVersionAtILInstruction(
-			
-			// BNHighLevelILFunction* func
-		    IntPtr func  , 
-			
-			// BNVariable* _var
-		    in BNVariable _var  , 
-			
-			// uint64_t instr
-			HighLevelILInstructionIndex instr  
-		);
+			IntPtr function,
+			in BNVariable variable,
+			HighLevelILInstructionIndex instruction);
 	}
 }
